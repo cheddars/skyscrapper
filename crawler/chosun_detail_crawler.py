0aab0388a40644d2727c29ee5e5dc166f3144622
cheddar <nezahrish@gmail.com>
2019-11-09 00:50:14 +0900

    not found

--- crawler/chosun_detail_crawler.py
+++ crawler/chosun_detail_crawler.py
@@ -61,8 +61,9 @@ class ChosunDetailCrawler:
     response.encoding = "UTF-8"
     soup = BeautifulSoup(response.text, 'html.parser')
     tag = soup.find("div", class_="par")
+    title = soup.find("title")
 
-    if tag == None and soup.find("title").text == "요청하신 페이지를 찾지 못했습니다.":
+    if tag == None and title != None and title.text == "요청하신 페이지를 찾지 못했습니다.":
       print("Not Found")
       return "NOT_FOUND"
     elif tag != None and len(tag.text) > 10:
